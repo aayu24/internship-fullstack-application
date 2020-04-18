5f39a727e5c9f999dc7ff5e422a6b49dc6901fb1
aayu24 <patni170101001@iitg.ac.in>
2020-04-18 22:51:31 +0530

    Added cookie parser.

--- index.js
+++ index.js
@@ -22,6 +22,32 @@ class MyWriter {
     }
 }
 
+//function to get Cookie but not working
+function getCookie(request, name) {
+    // Return result
+    let result = null;
+    // Retrieve Cookie as string
+    let cookieString = request.headers.get('set-cookie');
+    // If cookieString gets valid cookie/not empty string
+    if (cookieString) {
+        // split all our cookies into an array w/o semicolons
+        let cookies = cookieString.split(';');
+        // loop through each cookie to search for cookie w/ name @param name
+        cookies.forEach(cookie => {
+            // get cookie name
+            let cookieName = cookie.split('=')[0].trim();
+            // check if current cookie is the cookie we want
+            if (cookieName === name) {
+                // get value of cookie
+                let cookieVal = cookie.split('=')[1];
+                result = cookieVal;
+            }
+        });
+    }
+    return result;
+}
+
+
 /**
  * Respond with hello worker text
  * @param {Request} request
@@ -31,7 +57,7 @@ async function handleRequest(request) {
         let response = await fetch(BASE_URL);
         let urlno = Math.floor(Math.random() * 2);
         let data = await response.json();
-        let request = new Request(data['variants'][urlno]);
+        let request = new Request(data.variants[urlno]);
         let result = await fetch(request);
         result = new Response(result.body, result);
         const mywriter = new HTMLRewriter()
